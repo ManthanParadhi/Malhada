c2a51cecbbddfffd6345b23c606abbc07d0acce1
ManthanParadhi <66413955+ManthanParadhi@users.noreply.github.com>
2021-06-02 09:45:51 +0530

    add

---
+++ core/system.py
@@ -0,0 +1,7 @@
+
+class Runner:
+    def __init__(self):
+        pass
+
+    def run(self, program):
+        pass
--- main1.py
+++ main1.py
@@ -8,6 +8,7 @@ import json
 import keyboard
 # import the core lib
 from core import SystemInfo
+from core.system import Runner
 # import classifier
 from NLU.classifier import classify
 
@@ -16,6 +17,19 @@ engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 # changing index, changes voices. 1 for female and 0 for male
 engine.setProperty('voice', voices[1].id)
+
+
+def evaluate(text):
+    entity = classify(text)
+    if entity == 'time\getTime':
+        speak(SystemInfo.get_time())
+    elif entity == 'time\getDate':
+        speak(SystemInfo.get_date())
+    elif entity == 'open\notepad':
+        speak('ok, opening notepad')
+        os.system('notepad.exe')
+    else:
+        pass
 
 
 def speak(text):
@@ -28,25 +42,17 @@ rec = KaldiRecognizer(model, 16000)
 
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1,
-                rate=16000, input=True, frames_per_buffer=8000)
+                rate=16000, input=True, frames_per_buffer=8192)
 stream.start_stream()
 
 
 while True:
-    data = stream.read(10000)
+    data = stream.read(8192)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
         # convert result from string to dict
         result = json.loads(rec.Result())
         text = result['text']
-
-        entity = classify(text)
-
-        if entity == 'time\getTime':
-            speak(SystemInfo.get_time())
-        elif entity == 'time\getDate':
-            speak(SystemInfo.get_date())
-        else:
-            pass
-        print(text, '\n')
+        print(text)
+        evaluate(text)
